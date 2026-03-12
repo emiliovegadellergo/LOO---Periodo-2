cc55a70cc9d8266b00b406130f75199cc76a6a72
Emilio <emiliovegadellergov@gmail.com>
2026-03-12 09:33:37 -0600

    Ejercicio de practica .cpp 12-03-26

---
+++ P2-Notas/Animales.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include ≤Animales.h>
+
+Mamifero::Mamifero(string n, double p) {
+    this -> nombre = n;
+    this -> peso = p;
+}
+
+void Mamifero::setNombre(string n) {
+    this -> nombre = n; 
+}
+string Mamifero::getNombre() {
+    return this -> nombre;
+}
+string Mamifero::toString() {
+    return "Mamifero\n [" + nombre + "," + to_string(peso) + " kg]";
+}
+
+/************************ CLASE GATO ********************/
+Gato::Gato(string , double p, int b) : Mamifero (n, p) {
+    this -> bigotes = b;
+}
+
+string Gato::toString() {
+    return "GATO"+Mamifero::toString() + " [Bigotes:" + to_string(nBigotes) + "]";
+}
+
+/************************ CLASE VACA ********************/
+Vaca::Vaca(string n, double p, double l) : Mamifero(n,p) {
+    this -> litrosDeLeche = l;
+}
+
+void Vaca::calcularCantidadComida() {
+    cout << this->peso * 0.03 << " kg de comida" << endl;
+}
+string Vaca::toString() {
+    return "VACA" + Mamifero::toString() + " [Litros de leche:" + to_string(litrosDeLeche) + "]";
+}
+
+/************************ CLASE BALLENA ********************/
+Ballena::Ballena(string n, double p) : mamifero(n,p) {
+}
+string Ballena::toString() {
+    return "BALLENA" + Mamifero::toString();
+}
---
+++ P2-Notas/testAnimales.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Animales.h"
+using namespace std;
+
+int main() {
+    Gato mitch ("Mitch", 6.7, 20);
+    Vaca nani ("Nani", 350, 20);
+    Ballena titi ("Titi", 200000);
+
+    cout << mitch.toString() << endl;
+
+    cout << nani.toString() << endl;
+    nani.calcularCantidadComida();
+    
+    cout << titi.toString() << endl;
+
+}
